60a304c791741c968579d35a034dc42878bc2848
Deborah Helen Bright. G <145212083+Deby-Bright@users.noreply.github.com>
2023-12-11 02:06:19 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,29 @@
+print("Select an operation to perform: ")
+print("1. ADD")
+print("2. SUBTRACT")
+print("3. MULTIPLY")
+print("4. DIVIDE")
+operation=input()
+
+if operation == "1" :
+    #code for addition
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The sum is "+ str(int(num1)+int(num2)))
+elif operation == "2" :
+    #code for subtraction
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The sum is "+ str(int(num1)-int(num2)))
+elif operation == "3" :
+    #code for multiplication
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The sum is "+ str(int(num1)*int(num2)))
+elif operation == "4" :
+    #code for division
+    num1=input("Enter first number: ")
+    num2=input("Enter second number: ")
+    print("The sum is "+ str(int(num1)/int(num2)))
+else: 
+    print("Invalid Entry")
