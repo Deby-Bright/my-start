e69e1fb6246a15513aed806ad2fd33c6b4a69c66
Deborah Helen Bright. G <145212083+Deby-Bright@users.noreply.github.com>
2023-12-11 20:28:12 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
-print("Select an operation to perform: ")
 print("1. ADD")
 print("2. SUBTRACT")
 print("3. MULTIPLY")
 print("4. DIVIDE")
+print("Select an operation to perform: ")
 operation=input()
 
 if operation == "1" :
